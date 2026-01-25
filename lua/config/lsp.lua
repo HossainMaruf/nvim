592960f788b37429e9f7cfa0b68181eda2e1f397
HossainMaruf <maruficepustian@gmail.com>
2026-01-25 17:47:05 +0600

    Updated LSP

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -17,8 +17,17 @@ vim.lsp.config("jdtls", {
 -- Enable the server
 vim.lsp.enable("jdtls")
 
-vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "gi", vim.lsp.buf.implementation)
 vim.keymap.set("n", "gr", vim.lsp.buf.references)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration)
 vim.keymap.set("n", "K", vim.lsp.buf.hover)
+
+-- "gra" (Normal and Visual mode) is mapped to vim.lsp.buf.code_action()
+-- "gri" is mapped to vim.lsp.buf.implementation()
+-- "grn" is mapped to vim.lsp.buf.rename()
+-- "grr" is mapped to vim.lsp.buf.references()
+-- "grt" is mapped to vim.lsp.buf.type_definition()
+-- "gO" is mapped to vim.lsp.buf.document_symbol()
+-- CTRL-S (Insert mode) is mapped to vim.lsp.buf.signature_help()
+-- "an" and "in" (Visual and Operator-pending mode) are mapped to outer and inner incremental selections, respectively, using vim.lsp.buf.selection_range() 
